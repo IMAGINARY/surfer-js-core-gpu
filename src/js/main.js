0f44043ae0a58adfa6dddef1dc7a1ded06c1f9e6
Christian Stussak <stussak@mfo.de>
2022-09-22 16:28:47 +0200

    Fix HTML and JS formatting

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-const input = document.getElementById('inp');
+const input = document.getElementById("inp");
 
 async function init() {
   CindyJS.registerPlugin(1, "surfer-js-core-gpu", (api) => {
@@ -6,33 +6,82 @@ async function init() {
       updateImplicitFunction(input.value);
     });
   });
-  window.cdy = CindyJS.newInstance({canvasname:"CSCanvas",
-              scripts: {
-                init: await (await fetch(new URL("./src/cindyscript/init.cindyscript", window.location.href))).text(),
-                draw: await (await fetch(new URL("./src/cindyscript/draw.cindyscript", window.location.href))).text(),
-                mousedown: await (await fetch(new URL("./src/cindyscript/mousedown.cindyscript", window.location.href))).text(),
-                mouseup: await (await fetch(new URL("./src/cindyscript/mouseup.cindyscript", window.location.href))).text(),
-              },
-              animation: {autoplay: true},
-              use : ["CindyGL", "symbolic", "surfer-js-core-gpu"],
-              geometry: [ { name:"PA", kind:"P", type:"Free", pos: [.5,.37,1], narrow: true, color: [1,1,1], size:8 },
-                          { name:"PB", kind:"P", type:"Free", pos: [.5,.5,1], narrow: true, color: [1,1,1], size:8 },
-                          { name:"PC", kind:"P", type:"Free", pos: [.5,.1,1], narrow: true, color: [1,1,1], size:8 } ],
-              ports: [{
-                id: "CSCanvas",
-                width: 700,
-                height: 500,
-                transform: [ { visibleRect: [ -0.7, -0.5, 0.7, 0.5 ] } ]
-              }]
+  window.cdy = CindyJS.newInstance({
+    canvasname: "CSCanvas",
+    scripts: {
+      init: await (
+        await fetch(
+          new URL("./src/cindyscript/init.cindyscript", window.location.href)
+        )
+      ).text(),
+      draw: await (
+        await fetch(
+          new URL("./src/cindyscript/draw.cindyscript", window.location.href)
+        )
+      ).text(),
+      mousedown: await (
+        await fetch(
+          new URL(
+            "./src/cindyscript/mousedown.cindyscript",
+            window.location.href
+          )
+        )
+      ).text(),
+      mouseup: await (
+        await fetch(
+          new URL("./src/cindyscript/mouseup.cindyscript", window.location.href)
+        )
+      ).text(),
+    },
+    animation: { autoplay: true },
+    use: ["CindyGL", "symbolic", "surfer-js-core-gpu"],
+    geometry: [
+      {
+        name: "PA",
+        kind: "P",
+        type: "Free",
+        pos: [0.5, 0.37, 1],
+        narrow: true,
+        color: [1, 1, 1],
+        size: 8,
+      },
+      {
+        name: "PB",
+        kind: "P",
+        type: "Free",
+        pos: [0.5, 0.5, 1],
+        narrow: true,
+        color: [1, 1, 1],
+        size: 8,
+      },
+      {
+        name: "PC",
+        kind: "P",
+        type: "Free",
+        pos: [0.5, 0.1, 1],
+        narrow: true,
+        color: [1, 1, 1],
+        size: 8,
+      },
+    ],
+    ports: [
+      {
+        id: "CSCanvas",
+        width: 700,
+        height: 500,
+        transform: [{ visibleRect: [-0.7, -0.5, 0.7, 0.5] }],
+      },
+    ],
   });
   window.cdy.startup();
 }
 
-
 function updateImplicitFunction(expr) {
-  cdy.evokeCS('fun(x,y,z) := (' + expr + '); init();');
+  cdy.evokeCS("fun(x,y,z) := (" + expr + "); init();");
 }
 
 init().then();
 
-input.addEventListener('keypress', (event) => event.code === 'Enter' ? updateImplicitFunction(input.value) : false);
+input.addEventListener("keypress", (event) =>
+  event.code === "Enter" ? updateImplicitFunction(input.value) : false
+);
