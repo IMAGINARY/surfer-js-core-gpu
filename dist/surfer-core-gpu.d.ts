b59daf3cb4077a7b7089960672d25c5817c2fff7
Christian Stussak <stussak@mfo.de>
2022-09-27 16:30:14 +0200

    Update build results

--- dist/surfer-core-gpu.d.ts
+++ dist/surfer-core-gpu.d.ts
@@ -1,15 +1,35 @@
+type NodeGenerator = (degree: number) => number[];
+declare class PolynomialInterpolation {
+    protected minDegree: number;
+    protected maxDegree: number;
+    protected nodeGenerator: NodeGenerator;
+    constructor(nodeGenerator?: NodeGenerator, maxDegree?: number, minDegree?: number);
+    getMinDegree(): number;
+    getMaxDegree(): number;
+    generateNodes(degree: number): number[];
+    static nodeGeneratorEquidistant(): NodeGenerator;
+    static nodeGeneratorChebyshev(): NodeGenerator;
+}
 export class SurferCoreGpu {
+    protected readonly api: CindyJS.ApiV1;
     protected readonly cdy: CindyJS;
     readonly element: HTMLElement;
     readonly canvas: HTMLCanvasElement;
+    protected algorithm: PolynomialInterpolation;
     protected expression: string;
+    protected twoSided: boolean;
     protected alpha: number;
     protected zoom: number;
     protected parameters: {
         [key: string]: number;
     };
+    static readonly Algorithms: {
+        readonly PolynomialInterpolation: typeof PolynomialInterpolation;
+    };
     private constructor();
+    getAlgorithm(): PolynomialInterpolation;
     getExpression(): string;
+    getTwoSided(): boolean;
     getAlpha(): number;
     getZoom(): number;
     getParameter(name: string): number | undefined;
@@ -18,9 +38,11 @@ export class SurferCoreGpu {
     };
     getParameterNames(): string[];
     setExpression(expression: string): this;
+    setTwoSided(hasTwoSides: boolean): this;
     setAlpha(alpha: number): this;
     setZoom(zoom: number): this;
     setParameter(name: string, value: number): this;
+    setAlgorithm(algorithm: PolynomialInterpolation): void;
     static create(container: HTMLElement, width?: number, height?: number): Promise<SurferCoreGpu>;
 }
 export default SurferCoreGpu;
